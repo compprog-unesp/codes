6d6bf3c004fbb15369b38eb6258979012a82366e
Eduardo Bomfim <111364629+eduduuu@users.noreply.github.com>
2025-04-01 17:30:44 -0300

    Adicionadas resoluções de 04/02

---
+++ compprog/2025-04-02/630A.cpp
@@ -0,0 +1,13 @@
+// Os últimos dígitos de 5^n com n >= 2 sempre serão 25.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long int n; cin >> n;
+
+    cout << 25;
+
+    return 0;
+}
